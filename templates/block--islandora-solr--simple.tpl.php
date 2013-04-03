0ec252ce07217d65236d4b370060bc1b49f4aaaf
Favenzio Calvo <fcalvo@fsu.edu>
2013-04-03 14:02:41 -0400

    Fixed facet display plus/minus problem. Added View All Objects button to Basic Search.

---
+++ templates/block--islandora-solr--simple.tpl.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @file block--content--display-switch.tpl.php
+ * This file overrides Adaptivethemes block.tpl.php implementation for the display switch in Islandora's simple search.
+ *
+ * Adaptivetheme supplied variables:
+ * - $outer_prefix: Holds a conditional element such as nav, section or div and
+ *                  includes the block id, classes and attributes.
+ * - $outer_suffix: Closing element.
+ * - $inner_prefix: Inner div with .block-inner and .clearfix classes.
+ * - $inner_suffix: Closing div.
+ * - $title: Holds the block->subject.
+ * - $content_processed: Pre-wrapped in div and attributes, but for some
+ *   blocks these are stripped away, e.g. menu bar and main content.
+ * - $is_mobile: Bool, requires the Browscap module to return TRUE for mobile
+ *   devices. Use to test for a mobile context.
+ *
+ * Available variables:
+ * - $block->subject: Block title.
+ * - $content: Block content.
+ * - $block->module: Module that generated the block.
+ * - $block->delta: An ID for the block, unique within each module.
+ * - $block->region: The block region embedding the current block.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - block: The current template type, i.e., "theming hook".
+ *   - block-[module]: The module generating the block. For example, the user
+ *     module is responsible for handling the default user navigation block. In
+ *     that case the class would be 'block-user'.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Helper variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $block_zebra: Outputs 'odd' and 'even' dependent on each block region.
+ * - $zebra: Same output as $block_zebra but independent of any block region.
+ * - $block_id: Counter dependent on each block region.
+ * - $id: Same output as $block_id but independent of any block region.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ * - $block_html_id: A valid HTML ID and guaranteed unique.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_block()
+ * @see template_process()
+ * @see adaptivetheme_preprocess_block()
+ * @see adaptivetheme_process_block()
+ */
+?>
+<section id="block-islandora-solr-simple" class="block block-islandora-solr">
+  <div class="block-inner clearfix">
+    <h2 class="block-title">Search the Repository</h2>
+      <?php print $content ?>
+      <form action="/islandora/search/?type=dismax" method="post" id="islandora-solr-view-all-objects">
+        <input type="submit" id="all-objects-submit" name="all-objects" value="view all objects" class="form-submit" />
+      </form>
+  </div>
+</section>
---
+++ templates/islandora-solr-facet.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Template file for default facets
+ *
+ * @TODO document available variables
+ */
+?>
+
+<table>
+  <?php foreach($buckets as $key => $bucket): ?>
+    <tr>
+      <td>
+      <?php print $bucket['link']; ?>
+      <span class="count">(<?php print $bucket['count']; ?>)</span>
+      </td>
+      <td>
+      <span class="plusminus">
+        <?php print $bucket['link_plus']; ?>
+        <?php print $bucket['link_minus']; ?>
+      </span>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+</table>
