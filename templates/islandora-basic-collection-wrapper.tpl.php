f2ba0d18baeae3f70a035b8db3b087bd025e10d8
Favenzio Calvo <fcalvo@fsu.edu>
2013-05-08 15:47:24 -0400

    Added local collection search functionality.

--- templates/islandora-basic-collection-wrapper.tpl.php
+++ templates/islandora-basic-collection-wrapper.tpl.php
@@ -20,13 +20,34 @@
     else {
       print '<h1 id="page-title">' . $islandora_object->label . '</h1>';
     }
+  ?>
+  
+  <?php
     
     if (isset($islandora_object['DESC-TEXT'])) {
       $description_text_url = '/islandora/object/' . $islandora_object->id . '/datastream/DESC-TEXT/view'; 
       $description_text = file_get_contents($base_url . $description_text_url);
-      print '<p>' . $description_text . '</p>';
+      $search_url = '/islandora/object/' . $islandora_object->id;
+      print '<table id="local-collection-search"><tr><td><p>' . $description_text . '</p></td>'; ?>
+      
+      <td id="local-search-container">
+      <h2 class="local-collection-search-title">Search this Collection</h2>
+      <form id="local-collection-search-form" accept-charset="UTF-8" method="post" action="<?php print $search_url ?>">
+        <div class="form-item">
+          <input class="search-input-text" type="text" maxlength="128" name="islandora_simple_search_query" />
+          <input type="submit" name="submit" value="search"  class="form-submit" />
+        </div>
+          <input type="hidden" name="islandora_simple_collection" value="<?php print $islandora_object->id ?>">
+          <input type="hidden" value="islandora_solr_simple_search_form" name="form_id">
+      </form>
+      </td>
+      </tr>
+      </table>
+
+  <?php
     }
   ?>
+  
   <div class="islandora-basic-collection clearfix">
     <span class="islandora-basic-collection-display-switch">
      <?php print theme('links', array('links' => $view_links, 'attributes' => array('class' => array('links', 'inline'))));?>
