a944b572f70d29d8fc8534a798fd83f689bd3a88
Favenzio Calvo <fcalvo@fsu.edu>
2013-05-08 13:35:24 -0400

    Improved display of shortened descriptions.

--- templates/islandora-basic-collection.tpl.php
+++ templates/islandora-basic-collection.tpl.php
@@ -28,7 +28,7 @@
 
 <div class="islandora islandora-basic-collection">
     <?php $row_field = 0; ?>
-    <?php $description_length = 250; ?>
+    <?php $description_length = 300; ?>
     <?php foreach($associated_objects_mods_array as $associated_object): ?>
       <div class="islandora-basic-collection-object islandora-basic-collection-list-item clearfix"> 
         <dl class="<?php print $associated_object['class']; ?>">
@@ -43,13 +43,15 @@
               <?php endif; ?>
             </dd>
             <dd>
-              <?php if (isset($associated_object['collection_description'])): ?>
-                <strong>Description: </strong><?php print substr($associated_object['collection_description'], 0, $description_length); ?>
+              <?php if (isset($associated_object['collection_description']) && strlen($associated_object['collection_description']) <= $description_length) : ?>
+                <strong>Description: </strong><?php print $associated_object['collection_description']; ?>
+              <?php elseif (isset($associated_object['collection_description'])): ?>
+                <strong>Description: </strong><?php print substr($associated_object['collection_description'], 0, $description_length) . '...'; ?>
               <?php endif; ?>
             </dd>
             <?php if (isset($associated_object['mods_array']['mods:description']['value']) && $associated_object['mods_array']['mods:description']['value'] != ''): ?>
               <dd class="collection-value <?php print $associated_object['mods_array']['mods:description']['class']; ?>">
-                <strong><?php print $associated_object['mods_array']['mods:description']['label']; ?>: </strong><?php print substr($associated_object['mods_array']['mods:description']['value'], 0, $description_length); ?>
+                <strong><?php print $associated_object['mods_array']['mods:description']['label']; ?>: </strong><?php print $associated_object['mods_array']['mods:description']['short_value']; ?>
               </dd>
             <?php endif; ?>
             <?php if (isset($associated_object['mods_array']['mods:date']['value']) && $associated_object['mods_array']['mods:date']['value'] != ''): ?>
