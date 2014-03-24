8ef60fb3083b24d3b944972b54851df7afde6069
Gail Lewis <fclgnl@islandorad.fcla.edu>
2014-03-24 10:09:43 -0400

    Include file for  Logaholic javascript snippet - different for dev, test, and production.

--- templates/page--islandora--object.tpl.php
+++ templates/page--islandora--object.tpl.php
@@ -1,18 +1,4 @@
-<!-- /* Logaholic Web Analytics Code */ --><script type="text/javascript">
-var lwa_id = "LWA_p50";
-if (document.location.protocol=="https:") { var ptcl = "https:" } else { var ptcl = "http:" }
-var lwa_server = ptcl + "//statistics.flvc.org\/";
-document.write(unescape("%3Cscript type='text/javascript' src='" + lwa_server + "lwa.js'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var lwa = trackPage();
-</script>
-<noscript><a href="http://statistics.flvc.org\/logaholictracker.php?conf=LWA_p50"><img src="http://statistics.flvc.org\/logaholictracker.php?conf=LWA_p50" alt="web stats" border="0" /></a> <a href="http://www.logaholic.com/">Web Analytics</a> by Logaholic</noscript>
-<script type='text/javascript'>
-var lg_url = 'http://statistics.flvc.org\/';
-var lg_profile = 'Islandora';
-</script>
-<script type='text/javascript' src='http://statistics.flvc.org\/inpage.js' ></script>
+<?php include_once 'logaholic.php'; ?>
 <?php // Corolla ?>
 <div id="page-wrapper">
   <div id="page" class="<?php print $classes; ?>">
