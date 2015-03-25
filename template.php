757515c51ac1f1dcc8a4a4fdc34158c12c20fbaf
Favenzio Calvo <fcalvo@fsu.edu>
2015-03-25 16:27:58 -0400

    Added jQuery accordion functionality

--- template.php
+++ template.php
@@ -26,6 +26,7 @@ function islandoratheme_variables(&$vars) {
  */
 function islandoratheme_preprocess_html(&$vars) {
   drupal_add_library ('system', 'ui.tabs');
+  drupal_add_library ('system', 'ui.accordion');
   //drupal_add_js('jQuery(document).ready(function(){jQuery("#tabs").tabs();});', 'inline');
   drupal_add_js('jQuery(document).ready(function(){jQuery("#tabs").tabs();if (typeof _currentContentModel !== "undefined" && _currentContentModel && _currentContentModel=="islandora:compoundCModel" && (window.location.href.indexOf("#tabs") < 0) &&(window.location.href.indexOf("tabload=true") < 0)) {jQuery("#tabs").tabs("select", jQuery("#tabs").tabs("length")-1);} });', 'inline');  
   drupal_add_js('jQuery(document).ready(function(){ jQuery("#islandora-solr-metadata-browse-form").submit(function () { jQuery("#edit-browse-field").removeAttr("autocomplete"); }); });', 'inline');
