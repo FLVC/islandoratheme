3c331221b6461167dab493775721e80e01c22d19
Gail Lewis <fclgnl@islandorad.fcla.edu>
2017-07-29 19:00:18 -0400

    Use thumbnail_overlay instead of embargo_overlay for Organization and Person thumbnails.

--- js/entity_overlay.js
+++ js/entity_overlay.js
@@ -1,7 +1,7 @@
 // Make embargoed thumbnails obvious.
 jQuery(document).ready(function($) {
   $('.flvc_content_model_organization_tn').wrap('<div style="position:relative;"></div>');
-  $('.flvc_content_model_organization_tn').after('<span id="embargo_overlay">Organization Entity</span>');
+  $('.flvc_content_model_organization_tn').after('<span id="thumbnail_overlay">Organization Entity</span>');
   $('.flvc_content_model_person_tn').wrap('<div style="position:relative;"></div>');
-  $('.flvc_content_model_person_tn').after('<span id="embargo_overlay">Person Entity</span>');
+  $('.flvc_content_model_person_tn').after('<span id="thumbnail_overlay">Person Entity</span>');
 });
