4de14b389c5a48b9d369f0e26cbe4c7a93724806
Brown, Bryan <bryjbrown@gmail.com>
2017-08-16 15:46:20 -0400

    Fixes tabs for newspapers that are part of compound objects

--- templates/islandora-internet-archive-bookreader.tpl.php
+++ templates/islandora-internet-archive-bookreader.tpl.php
@@ -131,6 +131,68 @@
     </div>
 </div>
 
+<?php if (isset($parent_mods_array)): ?>
+<div id="tabs-3">
+    <div class="islandora-pdf-image-sidebar">
+      <div>
+	<table class="islandora-table-display" width="100%">
+        <tbody>
+        <?php $row_field = 0; ?>
+        <?php foreach($parent_mods_array as $key => $value): ?>
+
+          <?php if(trim($value['value']) != ''): ?>
+
+            <tr class="islandora-definition-row">
+            <th class="full-description-heading<?php print $row_field == 0 ? ' first' : ''; ?>">
+              <?php print $value['label']; ?>:
+            </th>
+            <td class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+              <?php print $value['value']; ?>
+            </td>
+
+            <?php if($row_field == 0): ?>
+                <td class="islandora-basic-image-thumbnail" rowspan="8">
+                <?php if(isset($parent_thumbnail_img)): ?>
+                  <?php print $parent_thumbnail_img; ?>
+                <?php elseif(isset($islandora_full_url)): ?>
+                  <?php print l($islandora_thumbnail_img, $islandora_full_url, array('html' => TRUE)); ?>
+                <?php elseif(isset($islandora_thumbnail_img)): ?>
+                  <?php print $islandora_thumbnail_img; ?>
+                <?php endif; ?>
+                </td>
+            <?php endif; ?>
+            </tr>
+
+            <?php $row_field++; ?>
+
+          <?php endif; ?>
+
+        <?php endforeach; ?>
+        </tbody>
+        </table>
+      </div>
+      <?php if($parent_collections): ?>
+        <div>
+          <h2>In Collections</h2>
+          <ul>
+            <?php foreach ($parent_collections as $collection): ?>
+               <?php if(substr($collection->id, 0, 5) == 'palmm'): ?>
+                  <li><?php print l($collection->label . " (PALMM)", "http://palmm.digital.flvc.org/islandora/object/{$collection->id}"); ?></li>
+               <?php elseif (strpos($base_url, 'palmm') !== false): ?>
+                  <?php $parsed_pid = explode(':',$collection->id); ?>
+                  <li><?php print l($collection->label, str_replace('palmm', $parsed_pid[0], $base_url) . "/islandora/object/{$collection->id}"); ?></li>
+               <?php else: ?>
+                 <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+               <?php endif; ?>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      <?php endif; ?>
+    </div>
+</div>
+
+<?php endif; ?>
+
 <?php if (isset($newspaper_mods_array)): ?>
 <div id="tabs-3">
     <div class="islandora-book-sidebar">
