69d9cddaea3e768fda5fcb868e76e3c08d1fd33d
glewis@uwf.edu <glewis@uwf.edu>
2020-08-18 17:40:08 -0400

    Organization display - scholar by department view: do not display missing scholar thumbnails.

--- js/islandora_dept.js
+++ js/islandora_dept.js
@@ -1,9 +1,19 @@
 jQuery(document).ready( function($) {
+
   lazyload();
+  $('#datatable-1 img').error(function () {
+      $(this).addClass('missing-scholar');
+  });
+
   jQuery('#datatable-1').dataTable().fnSettings().aoDrawCallback.push( {
     "fn": function(oSettings) {
               lazyload();
+              $('#datatable-1 img').error(function () {
+                 $(this).addClass('missing-scholar');
+              });
+
           },
     "sName": "user"
   } );
+
 });
